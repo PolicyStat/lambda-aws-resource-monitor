967ce01c32c1d4ac48aa0069efab82ff6d22d6c8
Kyle Gibson <kyle.gibson@frozenonline.com>
2019-02-12 18:37:56 -0500

    fixed deploy script, updated template

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+DEPLOY_ENV=$(pwd)/.deploy
 echo 'creating virtualenv' &&
-virtualenv --quiet .deploy &&
-export PATH=$(pwd)/.deploy/bin &&
+rm -fR $DEPLOY_ENV &&
+virtualenv --quiet $DEPLOY_ENV &&
+export VIRTUAL_ENV=$DEPLOY_ENV
+export PATH=$VIRTUAL_ENV/bin &&
 echo 'installing requirements' &&
 pip -q install -r requirements.txt &&
 echo 'deploying to lambda' &&
